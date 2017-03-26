a5d7ca364cd2fbb377e2917373888805bd024349
sergeevAlex <alexsergeev@i.ua>
2017-03-26 18:49:28 +0300

    Modalwindowadded

--- lab2.js
+++ lab2.js
@@ -1,8 +1,22 @@
+function() t_work{
+    var number_tr = document.getElementById('ntriangles').value;
+    var c = 3;
+    var array = [];
+    for(var i =0;i< number_tr;i++){
+        array[i] = [];
+        for(var j=0;j<c;j++){
+            array[i][j] = 1;
+        }
+        
+    }
+    
+    alert(array.length);
+    
+}
 function f_print(Str) {
     var div1 = document.getElementById("div1");
     div1.innerHTML += Str + "<br>";
 }
-
 function read_sides() {
     var a = document.getElementById('first').value;
     var b = document.getElementById('second').value;
@@ -10,4 +24,3 @@ function read_sides() {
     var html = 'a = <b>' + a  + '</b> b = <b>' + b + '</b> c = ' + c ; 
     document.getElementById('result').innerHTML = html;
 }
-
